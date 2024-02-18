f88c8c101e88b1030a1d8fe1c74f87de864bfe33
mixfon <mixfon@rambler.ru>
2024-02-18 13:06:12 +0300

    Внес изменения в сцену

--- MixPuzzle/MixPuzzle/AppDelegate.swift
+++ MixPuzzle/MixPuzzle/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		let viewController = GameViewController()
+		let viewController = MenuController()
 		let window = UIWindow(frame: UIScreen.main.bounds)
 		window.rootViewController = viewController
 		self.window = window
