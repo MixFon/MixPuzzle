ff22ba296d944b0e5160b7de01ea0e2adb7650e3
mixfon <mixfon@rambler.ru>
2024-04-17 22:55:10 +0300

    Накинул обработку активности кнопки

--- MixPuzzle/MixPuzzle/Scenes/SettingsCubeView/View/SettingsCubeView.swift
+++ MixPuzzle/MixPuzzle/Scenes/SettingsCubeView/View/SettingsCubeView.swift
@@ -6,9 +6,25 @@
 //
 
 import SwiftUI
+import Combine
 
 class SettingsCubeDependency: ObservableObject {
-	@Published var radius: Double = 10
+	@Published var radiusImage: Double = 10
+	@Published var chamferRadius: Double = 1
+	@Published var isButtonActive: Bool = false
+	
+	private var cancellables: Set<AnyCancellable> = []
+	
+	init() {
+		// Создаём поток, который срабатывает при изменении любого из двух свойств
+		Publishers.CombineLatest($radiusImage, $chamferRadius)
+			.map { radiusImage, chamferRadius in
+				// Проверяем условия активации кнопки
+				radiusImage > 10 || chamferRadius > 1
+			}
+			.assign(to: \.isButtonActive, on: self)
+			.store(in: &cancellables)
+	}
 }
 
 struct SettingsCubeView: View {
@@ -16,29 +32,33 @@ struct SettingsCubeView: View {
 	@ObservedObject var dependecy: SettingsCubeDependency = SettingsCubeDependency()
 	
 	@Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-	//@State var radius: Double = 1.0
 	
 	var body: some View {
 		VStack {
-			NavigationBar(title: "sdf")
+			NavigationBar(title: "Settings Cubes")
 				.padding()
 			SettingsCubeWrapper(dependency: dependecy)
 				.aspectRatio(contentMode: .fit)
 				.cornerRadius(10)
-				.padding()
 				.background(Color.mm_background_secondary)
-			OptionsSectionsView(title: "Cube", cells: [
-				AnyView(SliderCellView(title: "Radius", range: 0...30, radius: $dependecy.radius)),
-			])
-			.padding()
-			.toolbar {
-				BackButtonToolbarItem {
-					self.presentationMode.wrappedValue.dismiss()
-				}
+			Button {
+				
+			} label: {
+				Image(systemName: "sdcard")
+					.resizable()
+					.scaledToFit()
+					.frame(width: 32, height: 32)
+				
 			}
-			.navigationBarBackButtonHidden()
-			.navigationTitle("Settings Cubes")
-			.buttonStyle(.plain)
+			.disabled(dependecy.isButtonActive)
+
+			ScrollView {
+				OptionsSectionsView(title: "Cube", cells: [
+					AnyView(SliderCellView(title: "Image Radius", range: 0...25, radius: $dependecy.radiusImage)),
+					AnyView(SliderCellView(title: "Chamfer Radius", range: 0...2, radius: $dependecy.chamferRadius)),
+				])
+			}
+			.padding()
 			Spacer()
 		}
 		.background(Color.mm_background_secondary)
