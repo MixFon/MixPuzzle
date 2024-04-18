b5b16a3a4043fb688a370baafdbb301683617f71
mixfon <mixfon@rambler.ru>
2024-04-18 23:58:29 +0300

    Прописал роутер

--- MixPuzzle/MixPuzzle/Scenes/Options/OptionsView.swift
+++ MixPuzzle/MixPuzzle/Scenes/Options/OptionsView.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 
+final class OptionsViewRouter: ObservableObject {
+	@Published var toBox: Bool = false
+	@Published var toStars: Bool = false
+	@Published var toLanguage: Bool = false
+	@Published var toVibration: Bool = false
+}
+
 struct OptionsView: View {
 	
-	@State private var toBox: Bool = false
-	@State private var toStars: Bool = false
-	@State private var toLanguage: Bool = false
-	@State private var toVibration: Bool = false
+	@ObservedObject private var router = OptionsViewRouter()
+	
 	@State private var isOnVibration: Bool = false
 	
 	var body: some View {
@@ -20,22 +25,22 @@ struct OptionsView: View {
 			NavigationBar(title: "Options")
 				.padding()
 			OptionsSectionsView(title: "Garaphics", cells: [
-				AnyView(CellView(icon: "cube", text: "Cube", action: { self.toBox = true })),
-				AnyView(CellView(icon: "moon.stars", text: "Stars", action: { self.toStars = true })),
+				AnyView(CellView(icon: "cube", text: "Cube", action: { self.router.toBox = true })),
+				AnyView(CellView(icon: "moon.stars", text: "Stars", action: { self.router.toStars = true })),
 			])
 			.padding()
 			OptionsSectionsView(title: "Application", cells: [
-				AnyView(CellView(icon: "globe", text: "Language", action: { self.toLanguage = true })),
+				AnyView(CellView(icon: "globe", text: "Language", action: { self.router.toLanguage = true })),
 				AnyView(ToggleCellView(icon: "waveform.path", text: "Vibration", isOn: $isOnVibration)),
 			])
 			.padding()
 			Spacer()
 		}
 		.background(Color.mm_background_secondary)
-		.fullScreenCover(isPresented: self.$toBox) {
+		.fullScreenCover(isPresented: $router.toBox) {
 			SettingsCubeView()
 		}
-		.fullScreenCover(isPresented: self.$toStars) {
+		.fullScreenCover(isPresented: $router.toStars) {
 			SettingsCubeView()
 		}
 	}
