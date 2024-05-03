a889278eb29177112c9ed1224ef8a76d427922f8
mixfon <mixfon@rambler.ru>
2024-05-03 13:24:38 +0300

    Убрал коменты

--- MixPuzzle/MixPuzzle/Draft/Snackbar.swift
+++ MixPuzzle/MixPuzzle/Draft/Snackbar.swift
@@ -7,14 +7,6 @@
 
 import SwiftUI
 
-/// Specifies the `Snackbar`'s background color
-/// - `.default`:  the app's accent color
-/// - `.warning`: ocra
-/// - `.error`: red
-/// - `.custom(_: Color)`:  lets you specify the color you want
-/// - Author: Luca Zani
-/// - Version: 0.1
-@available(iOS 14.0, *)
 public enum MMSnackbarStyle {
 	case error
 	case warning
