32810ce3ef199795e4d61364e840a12f83803071
mixfon <mixfon@rambler.ru>
2025-01-12 14:14:32 +0300

    Добавил иконки в екстеншен Image

---
+++ MixPuzzle/MixPuzzle/Extentions/Ext+Image.swift
@@ -0,0 +1,31 @@
+//
+//  Ext+Image.swift
+//  MixPuzzle
+//
+//  Created by Михаил Фокин on 29.12.2024.
+//
+
+import SwiftUI
+
+extension Image {
+	
+	static var mix_icon_close: Image {
+		return Image("mix.icon.close", bundle: Bundle.main)
+	}
+	
+	static var mix_icon_help: Image {
+		return Image("mix.icon.help", bundle: Bundle.main)
+	}
+	
+	static var mix_icon_menu: Image {
+		return Image("mix.icon.menu", bundle: Bundle.main)
+	}
+	
+	static var mix_icon_restart: Image {
+		return Image("mix.icon.restart", bundle: Bundle.main)
+	}
+	
+	static var mix_icon_updates: Image {
+		return Image("mix.icon.updates", bundle: Bundle.main)
+	}
+}
---
+++ MixPuzzle/MixPuzzle/Extentions/Ext+UIImage.swift
@@ -0,0 +1,8 @@
+//
+//  Ext+UIImage.swift
+//  MixPuzzle
+//
+//  Created by Михаил Фокин on 29.12.2024.
+//
+
+import Foundation
