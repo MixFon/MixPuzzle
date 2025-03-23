6b562fff116a7f3e7afe01e58f73d473eb84938c
mixfon <mixfon@rambler.ru>
2025-03-23 12:28:04 +0300

    Удалил лишнее добавил файл локализации

--- MixPuzzle/MixPuzzle/Draft/SwiftUIView.swift
+++ MixPuzzle/MixPuzzle/Draft/SwiftUIView.swift
@@ -1,40 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MixPuzzle
+//
+//  Created by Михаил Фокин on 23.03.2025.
+//
+
 import SwiftUI
 
-struct AlertView: View {
-	@State private var showActionSheet = false
-	
-	var body: some View {
-		VStack {
-			Button("Показать алерт") {
-				showActionSheet = true
-			}
-			.actionSheet(isPresented: $showActionSheet) {
-				ActionSheet(
-					title: Text("Выберите действие"),
-					message: Text("Что вы хотите сделать?"),
-					buttons: [
-						.default(Text("Построить"), action: {
-							// Действие для кнопки "Построить"
-							debugPrint("Построить выбрано")
-						}),
-						.default(Text("Повысить уровень"), action: {
-							// Действие для кнопки "Повысить уровень"
-							debugPrint("Повысить уровень выбрано")
-						}),
-						.default(Text("Выбрать другой тип"), action: {
-							// Действие для кнопки "Выбрать другой тип"
-							debugPrint("Выбрать другой тип выбрано")
-						}),
-						.cancel()
-					]
-				)
-			}
-		}
-	}
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		AlertView()
-	}
+#Preview {
+    SwiftUIView()
 }
