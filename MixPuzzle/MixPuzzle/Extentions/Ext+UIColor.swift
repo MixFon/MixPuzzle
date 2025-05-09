2a759ccc2a27811331b6d77ae2119e45fd6960d1
mixfon <mixfon@rambler.ru>
2025-05-09 18:26:18 +0300

    Цвета для океана

---
+++ MixPuzzle/MixPuzzle/Extentions/Ext+UIColor.swift
@@ -0,0 +1,29 @@
+//
+//  Ext+UIColor.swift
+//  MixPuzzle
+//
+//  Created by Михаил Фокин on 09.05.2025.
+//
+
+import UIKit
+
+extension UIColor {
+	static let oceanColors: [UIColor] = [
+		   // Стандартный синий океан (HEX: #005594)
+		   UIColor(red: 0.0, green: 0.33, blue: 0.58, alpha: 1.0),
+		   // Тропический бирюзовый (HEX: #2EABD6)
+		   UIColor(red: 0.18, green: 0.67, blue: 0.84, alpha: 1.0),
+		   // Глубокий морской синий (HEX: #002266)
+		   UIColor(red: 0.0, green: 0.13, blue: 0.4, alpha: 1.0),
+		   // Серо-голубой (штормовой) (HEX: #457394)
+		   UIColor(red: 0.27, green: 0.45, blue: 0.58, alpha: 1.0),
+		   // Изумрудный (HEX: #00868F)
+		   UIColor(red: 0.0, green: 0.53, blue: 0.56, alpha: 1.0),
+		   // Светло-бирюзовый (HEX: #40E0D0)
+		   UIColor(red: 0.25, green: 0.88, blue: 0.82, alpha: 1.0),
+		   // Темный аквамарин (HEX: #01796F)
+		   UIColor(red: 0.0, green: 0.47, blue: 0.44, alpha: 1.0),
+		   // Полуночный синий (HEX: #191970)
+		   UIColor(red: 0.1, green: 0.1, blue: 0.44, alpha: 1.0)
+	   ]
+}
