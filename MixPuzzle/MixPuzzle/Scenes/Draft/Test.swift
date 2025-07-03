cb1a473b8b777c22680a56624dd74688608064ee
mixfon <mixfon@rambler.ru>
2025-07-03 21:42:27 +0300

    Коммит

---
+++ MixPuzzle/MixPuzzle/Scenes/Draft/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  MixPuzzle
+//
+//  Created by Михаил Фокин on 03.06.2025.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
